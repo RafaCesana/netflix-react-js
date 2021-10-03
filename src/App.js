6ab104659c80572413242c8a3b9740ee9e1f8219
Rafael Cesana <cesana.rafael@gmail.com>
2021-10-03 16:32:27 -0300

    Taking the movies lists to my App
    testing with console.log. It's working.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Tmdb from './Tmdb';
 
 export default() =>
 {
+
+  useEffect( 
+    ()=>
+    {
+      const loadAll = async() =>
+      {
+        // Tanking all the movies lists
+        let list = await Tmdb.getHomeList();
+        console.log(list);
+      }
+      loadAll();
+    },
+    []);
+
   return (
     <div>
       first test
