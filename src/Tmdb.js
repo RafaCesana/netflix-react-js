ef2ff61a660e899f602d6f337cfdda475f6bb806
Rafael Cesana <cesana.rafael@gmail.com>
2021-10-02 21:50:27 -0300

    Create Tmdb.js

---
+++ src/Tmdb.js
@@ -0,0 +1,69 @@
+const API_KEY = '2fc08aed66efc897b046fd2922def253';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+/*
+- originais da netflix
+- recomendados (trending)
+- em alta (top rated)
+- ação
+- comédia
+- terror
+- romance
+- documentários
+*/
+
+const basicFetch = async(endPoint) =>
+{
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default
+{
+    getHomeList: async() =>
+    {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: []
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para Você',
+                items: []
+            },
+            {
+                slug: 'toprated',
+                title: 'Em Alta',
+                items: []
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: []
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: []
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: []
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: []
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentário',
+                items: []
+            },
+        ];
+    }
+}
